55adb7db6e2068a7bc0970eb881d9fb9f4d7b7bd
flyingbeat <jordi.kueffer@uzh.ch>
2023-06-05 17:50:28 +0200

    adding api url to github variable

--- artis_api.py
+++ artis_api.py
@@ -20,18 +20,11 @@ class ViolationType(Enum):
 class ArtisAPI:
 
     def __init__(self):
-        self.base_url = os.environ.get('ARTIS_API_URL')
+        self.base_url = self.__getApiUrl()
         self.__signing_key = os.environ.get('ARTIS_LOGGER_SIGNING_KEY')
         self.artwork_id = os.environ.get('ARTIS_LOGGER_ARTWORK_ID')
         # self.logger_address = os.environ.get('ARTIS_LOGGER_ADDRESS')
         # self.did = f'did:ethr:{self.logger_address}'
-    
-    def __refresh_token(self):
-        get_exp = lambda token: json.loads(base64.b64decode(token.split('.')[1]).decode("utf-8")).get('exp')
-        token = os.environ.get('ARTIS_API_AUTH_TOKEN')
-        if token is None or get_exp(token) < datetime.datetime.now().timestamp():
-            os.environ['ARTIS_API_AUTH_TOKEN'] = issue_token(self.__signing_key)
-        return os.environ.get('ARTIS_API_AUTH_TOKEN')
 
     def report_violation(self, timestamp: int, violationType: ViolationType, value: float) -> None:
         # makin a patch request to the api
@@ -40,7 +33,7 @@ class ArtisAPI:
             'Content-Type': 'application/json',
             'Authorization': f'Bearer {self.__refresh_token()}'
             # 'Did': self.did,
-            # 'Signature': self._sign(self.did),
+            # 'Signature': self.__sign(self.did),
         }
         data = {
             'violationTimestamp': timestamp,
@@ -52,8 +45,28 @@ class ArtisAPI:
             print(f'Violation reported: {violationType.name} {value}')
             pprint(response.json())
 
+    def __getApiUrl(self) -> str:
+        """Get the api url from github actions secrets"""
+        access_token = os.environ.get("GITHUB_VARIABLES_ACCESS_TOKEN")
+        org_name = os.environ.get("GITHUB_ORG_NAME")
+        variable_name = os.environ.get("GITHUB_SC_ADDRESS_VARIABLE_NAME")
+        url = (
+            f"https://api.github.com/orgs/{org_name}/actions/variables/{variable_name}"
+        )
+        return (
+            requests.get(url, headers={"Authorization": f"Bearer {access_token}"})
+            .json()
+            .get("value")
+        )
+    
+    def __refresh_token(self):
+        get_exp = lambda token: json.loads(base64.b64decode(token.split('.')[1]).decode("utf-8")).get('exp')
+        token = os.environ.get('ARTIS_API_AUTH_TOKEN')
+        if token is None or get_exp(token) < datetime.datetime.now().timestamp():
+            os.environ['ARTIS_API_AUTH_TOKEN'] = issue_token(self.__signing_key)
+        return os.environ.get('ARTIS_API_AUTH_TOKEN')
 
-    def _sign(self, message: str) -> str:
+    def __sign(self, message: str) -> str:
         encoded = encode(['string'], [message])
         hashed = Web3.keccak(encoded)
 
